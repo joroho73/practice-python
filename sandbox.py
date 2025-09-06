97d1b88b84178d10ca38317a44a1add41b999243
John <joroho73@gmail.com>
2025-09-06 10:31:41 +0100

    added sandboxfile for tests and calculations

---
+++ sandbox.py
@@ -0,0 +1,14 @@
+# this file is used for testing out code snippets
+
+# ex_01.py
+
+print("01. num * num")
+num1 = int(input("Enter a number: "));
+print(num1 * num1);
+3
+
+print("02. number * a string")
+num2 = int(input("Enter a number: "));
+str2 = input("Enter a string: ");
+print(num2 * str2);
+
