21f4c5735f693ccc14e4878c3273fc59ecbd103c
John <joroho73@gmail.com>
2025-09-08 12:14:26 +0100

    condensed ex_04

--- ex_04.py
+++ ex_04.py
@@ -2,10 +2,9 @@
 # Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
 
 num = int(input("give me a number: "))
-a = range(1, num + 1)
 b = []
 
-for divisor in a:
+for divisor in range(1, num + 1):
     if num % divisor == 0:
         b.append(divisor)
 
